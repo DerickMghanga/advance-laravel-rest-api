34ae899554afaa841e2f9f657587e1a55ff9b99a
DerickMghanga <derickmghanga3329@gmail.com>
2025-01-12 15:53:13 +0300

    Recursively load PHP files in a directory

---
+++ app/Helpers/Routes/RouteHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Helpers\Routes;
+
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+class RouteHelper
+{
+    public static function includeStaticFiles(string $folder)
+    {
+        // iterate through the v1 folder recursively
+        $dirIterator = new RecursiveDirectoryIterator($folder);
+        /**
+         * @var RecursiveDirectoryIterator | RecursiveIteratorIterator $it
+         */
+        $it = new RecursiveIteratorIterator($dirIterator);
+
+        // require the file in each iteration
+        while ($it->valid()) {
+            if (
+                !$it->isDot()
+                && $it->isFile()
+                && $it->isReadable()
+                && $it->current()->getExtension() === 'php'
+            ) {
+                require $it->key();
+                // require $it->current()->getPathname();
+            }
+            $it->next();
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,16 @@
 <?php
 
+use App\Helpers\Routes\RouteHelper;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function () {
-    require __DIR__ . '/api/v1/users.php'; // adds all users routes from a file
-    require __DIR__ . '/api/v1/posts.php';
-    require __DIR__ . '/api/v1/comments.php';
+    RouteHelper::includeStaticFiles(__DIR__ . '/api/v1'); // this function iterates through a given folder with route files
+
+    // require __DIR__ . '/api/v1/users.php'; // adds all users routes from a file
+    // require __DIR__ . '/api/v1/posts.php';
+    // require __DIR__ . '/api/v1/comments.php';
 });
 
 Route::get('/user', function (Request $request) {
