c057047a1bac081b71cc144922d4d9ceaa95fa62
DerickMghanga <derickmghanga3329@gmail.com>
2025-02-06 21:03:34 +0300

    Test Driven Development  almost done

---
+++ app/Helpers/Math/ArithmeticHelper.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Helpers\Math;
+
+class ArithmeticHelper
+{
+    public static function add() {}
+    public static function minus() {}
+}
---
+++ tests/Unit/ArithmeticHelperTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ArithmeticHelperTest extends TestCase
+{
+    public function test_add_can_sum_numbers_up() {}
+    public function test_add_can_take_in_multiple_numbers() {}
+    public function test_add_can_only_take_in_numeric_arguments() {}
+    public function test_add_needs_at_least_one_argument() {}
+}
