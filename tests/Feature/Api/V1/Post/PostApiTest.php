32b2629cff4f85e8c5934a7d0761a45f426cc5b0
DerickMghanga <derickmghanga3329@gmail.com>
2025-02-06 16:51:52 +0300

    Comments and Users tests done

--- tests/Feature/Api/V1/Post/PostApiTest.php
+++ tests/Feature/Api/V1/Post/PostApiTest.php
@@ -2,8 +2,13 @@
 
 namespace Tests\Feature\Api\V1\Post;
 
+use App\Events\Models\Post\PostCreated;
+use App\Events\Models\Post\PostDeleted;
+use App\Events\Models\Post\PostUpdated;
 use App\Models\Post;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
 class PostApiTest extends  TestCase
@@ -40,16 +45,52 @@ class PostApiTest extends  TestCase
 
     public function test_create()
     {
+        Event::fake(); // catches the event / stops Laravel from dispatching any event in this test
         $dummy = Post::factory()->make();
 
         $response = $this->json('post', '/api/v1/posts', $dummy->toArray());
 
         $result = $response->assertStatus(201)->json('data');
+        Event::assertDispatched(PostCreated::class);  // only works is Event::fake() was called
 
         $result = collect($result)->only(array_keys($dummy->getAttributes()));
 
         $result->each(function ($value, $field) use ($dummy) {
             $this->assertSame(data_get($dummy, $field), $value, 'Fillable is not the same');
         });
+    }
+
+    public function test_update()
+    {
+        $dummy = Post::factory()->create();
+        $dummy2 = Post::factory()->make();
+        Event::fake();
+
+        $fillables = collect((new Post())->getFillable());
+
+        $fillables->each(function ($toUpdate) use ($dummy, $dummy2) {
+            $response = $this->json('patch', '/api/v1/posts/' . $dummy->id, [
+                $toUpdate => data_get($dummy2, $toUpdate),
+            ]);
+
+            $result = $response->assertStatus(200)->json('data');
+            Event::assertDispatched(PostUpdated::class);
+
+            $this->assertSame(data_get($dummy2, $toUpdate), data_get($dummy->refresh(), $toUpdate), "Failed to update the model");
+        });
+    }
+
+    public function test_delete()
+    {
+        Event::fake();
+        $dummy = Post::factory()->create();
+
+        $response = $this->json('delete', '/api/v1/posts/' . $dummy->id);
+
+        $result = $response->assertStatus(200);
+        Event::assertDispatched(PostDeleted::class);
+
+        $this->expectException(ModelNotFoundException::class);
+        Post::query()->findOrFail($dummy->id);
     }
 }
