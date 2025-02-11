83e65829b493a0e12c61926a4760796473f846dc
DerickMghanga <derickmghanga3329@gmail.com>
2025-02-11 19:39:26 +0300

    test function adds numbers

--- app/Helpers/Math/ArithmeticHelper.php
+++ app/Helpers/Math/ArithmeticHelper.php
@@ -4,6 +4,10 @@ namespace App\Helpers\Math;
 
 class ArithmeticHelper
 {
-    public static function add() {}
+    public static function add($num1, $num2)
+    {
+        return $num1 + $num2;
+    }
+    
     public static function minus() {}
 }
--- tests/Unit/ArithmeticHelperTest.php
+++ tests/Unit/ArithmeticHelperTest.php
@@ -2,12 +2,25 @@
 
 namespace Test\Unit;
 
+use App\Helpers\Math\ArithmeticHelper;
 use PHPUnit\Framework\TestCase;
 
 class ArithmeticHelperTest extends TestCase
 {
-    public function test_add_can_sum_numbers_up() {}
+    public function test_add_can_sum_numbers_up()
+    {
+        $num1 = 5;
+        $num2 = 10;
+        $sum = $num1 + $num2;
+
+        $result = ArithmeticHelper::add($num1, $num2);
+
+        $this->assertSame($sum, $result, 'Does not add numbers correctly');
+    }
+
     public function test_add_can_take_in_multiple_numbers() {}
+
     public function test_add_can_only_take_in_numeric_arguments() {}
+
     public function test_add_needs_at_least_one_argument() {}
 }
