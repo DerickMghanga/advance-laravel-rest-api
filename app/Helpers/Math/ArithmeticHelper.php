99b1c6907b750d2041de102fba6800aa366f8eca
DerickMghanga <derickmghanga3329@gmail.com>
2025-02-13 10:53:21 +0300

    est Driven Development basics done

--- app/Helpers/Math/ArithmeticHelper.php
+++ app/Helpers/Math/ArithmeticHelper.php
@@ -4,10 +4,39 @@ namespace App\Helpers\Math;
 
 class ArithmeticHelper
 {
-    public static function add($num1, $num2)
+    public static function add(...$nums)
     {
-        return $num1 + $num2;
+        if (sizeof($nums) < 1) {
+            throw new \InvalidArgumentException("Must have atleast 1 argument");
+        }
+
+        $sum = 0;
+        foreach ($nums as $num) {
+            if (!(is_float($num) || is_int($num))) {
+                throw new \InvalidArgumentException("Argument can only be numeric");
+            }
+            $sum += $num;
+        }
+        return $sum;
     }
-    
-    public static function minus() {}
+
+    public static function minus(...$nums)
+    {
+        if (sizeof($nums) < 1) {
+            throw new \InvalidArgumentException("Must have atleast 1 argument");
+        }
+
+        if (!(is_float($nums[0]) || is_int($nums[0]))) {
+            throw new \InvalidArgumentException("Argument can only be numeric");
+        }
+
+        $subtraction = $nums[0];
+        foreach (array_slice($nums, 1) as $num) {
+            if (!(is_float($num) || is_int($num))) {
+                throw new \InvalidArgumentException("Argument can only be numeric");
+            }
+            $subtraction -= $num;
+        }
+        return $subtraction;
+    }
 }
--- tests/Unit/ArithmeticHelperTest.php
+++ tests/Unit/ArithmeticHelperTest.php
@@ -3,7 +3,8 @@
 namespace Test\Unit;
 
 use App\Helpers\Math\ArithmeticHelper;
-use PHPUnit\Framework\TestCase;
+use InvalidArgumentException;
+use Tests\TestCase;
 
 class ArithmeticHelperTest extends TestCase
 {
@@ -18,9 +19,119 @@ class ArithmeticHelperTest extends TestCase
         $this->assertSame($sum, $result, 'Does not add numbers correctly');
     }
 
-    public function test_add_can_take_in_multiple_numbers() {}
+    public function test_add_can_take_in_multiple_numbers()
+    {
+        $num1 = 5;
+        $num2 = 10;
+        $num3 = 120;
+        $sum = $num1 + $num2 + $num3;
 
-    public function test_add_can_only_take_in_numeric_arguments() {}
+        $result = ArithmeticHelper::add($num1, $num2, $num3);
 
-    public function test_add_needs_at_least_one_argument() {}
+        $this->assertSame($sum, $result, 'Sum does not work when has multiple arguments');
+    }
+
+    public function test_add_cannot_take_in_string_argument()
+    {
+        // incase a wrong data type is sent
+        $this->expectException(InvalidArgumentException::class);
+
+        $result = ArithmeticHelper::add("abc");
+    }
+    public function test_add_cannot_take_in_array_argument()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $result = ArithmeticHelper::add(["abc"]);
+    }
+    public function test_add_cannot_take_in_null_argument()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $result = ArithmeticHelper::add(null);
+    }
+    public function test_add_cannot_take_in_boolean_argument()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $result = ArithmeticHelper::add(true);
+    }
+    public function test_add_cannot_take_in_function_argument()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $result = ArithmeticHelper::add(fn() => true);
+    }
+
+    public function test_add_needs_at_least_one_argument()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $result = ArithmeticHelper::add();
+    }
+
+    // ============================================
+    // MINUS TEST
+    // ============================================
+    public function test_minus_can_subtract_numbers()
+    {
+        $num1 = 25;
+        $num2 = 10;
+        $subtraction = $num1 - $num2;
+
+        $result = ArithmeticHelper::minus($num1, $num2);
+
+        $this->assertSame($subtraction, $result, 'Does not minus numbers correctly.');
+    }
+
+    public function test_minus_can_take_in_multiple_numbers()
+    {
+        $num1 = 25;
+        $num2 = 10;
+        $num3 = 1;
+        $sum = $num1 - $num2 - $num3;
+
+        $result = ArithmeticHelper::minus($num1, $num2, $num3);
+
+        $this->assertSame($sum, $result, "Sum does not work when has multiple arguments");
+    }
+
+    public function test_minus_cannot_take_in_string_arguments()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $result = ArithmeticHelper::minus("abc");
+    }
+
+    public function test_minus_cannot_take_in_array_argument()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $result = ArithmeticHelper::minus(["abc"]);
+    }
+
+    public function test_minus_cannot_take_in_null_argument()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $result = ArithmeticHelper::minus(null);
+    }
+
+    public function test_minus_cannot_take_in_boolean_argument()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $result = ArithmeticHelper::minus(true);
+    }
+
+    public function test_minus_cannot_take_in_function_argument()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $result = ArithmeticHelper::minus(fn() => true);
+    }
+
+    public function test_minus_needs_at_least_one_argument()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $result = ArithmeticHelper::minus();
+
+    }
 }
